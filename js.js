c7e763e3813c6d84b40a23ad5a2422aa4fcf25d5
hunterkiller <microsoftcompany1999@gmail.com>
2022-12-21 21:17:47 +0630

    finalproject

--- js.js
+++ js.js
@@ -4,8 +4,9 @@ const m = document.querySelector('#minutes');
 const s = document.querySelector('#seconds');
 const e = document.querySelector('h5');
 const container = document.querySelector('.container');
+const b = document.querySelector('body');
 
-const endDate = new Date(2023,0,1,00);
+const endDate = new Date(2022,11,21,00);
 const endTime = endDate.getTime();
 // console.log(Math.floor(endDate));
 const countdown = () => {
@@ -23,7 +24,24 @@ const countdown = () => {
     if(endTime < todayTime){
         clearInterval(i);
         container.style.display = 'none';
-        e.textContent = `Happy New Year & MALMAl`;
+        alert('Min Aung Hlaing is M.A.L');
+        b.style.backgroundImage = "URL('https://i.pinimg.com/736x/d4/33/39/d43339e81bf0e944af88665a75e6d8f3.jpg')";
+        e.innerHTML = `
+            <h6>
+                <span style="--i:1;">H</span>
+                <span style="--i:2;">A</span>
+                <span style="--i:3;">P</span>
+                <span style="--i:4;">P</span>
+                <span style="--i:5;">Y</span>
+                <span style="--i:6;">N</span>
+                <span style="--i:7;">E</span>
+                <span style="--i:8;">W</span>
+                <span style="--i:9;">Y</span>
+                <span style="--i:10;">E</span>
+                <span style="--i:11;">A</span>
+                <span style="--i:12;">R</span>
+            </h6>
+        `;
         return;
     }else{
         const daysLeft = Math.floor(reamingTime / oneDay ) ;
